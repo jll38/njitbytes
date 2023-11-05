cf8ddd32e6da6892e69cbc4a115e205fa321b753
Vladimir Gutierrez <vg377@njit.edu>
2023-11-05 11:55:08 -0500

    Updated

--- python/main.py
+++ python/main.py
@@ -1,14 +1,15 @@
-from datetime import datetime
-import os
-import requests
-import json
-from flask import Flask, jsonify
-from flask_basicauth import BasicAuth
 from dotenv import load_dotenv
-from typing import Any, List, Dict, Tuple
-from google.cloud import storage
+from flask import Flask, jsonify
+from datetime import datetime
+import requests
+import os
 import pytz
+from google.cloud import storage
+import json
+from flask_basicauth import BasicAuth
 
+app = Flask(__name__)
+basic_auth: BasicAuth = BasicAuth(app)
 load_dotenv()
 
 MEAL_PERIOD_MAPPING = {
@@ -17,10 +18,7 @@ MEAL_PERIOD_MAPPING = {
     "8f5b": "dinner",
 }
 
-cached_data: Dict[str, Any] = {}
-storage_client = storage.Client()
-app: Flask = Flask("NJIT Bytes")
-basic_auth: BasicAuth = BasicAuth(app)
+cached_menus = {}
 new_york = pytz.timezone("America/New_York")
 
 basic_auth_username: str = os.environ.get("BASIC_AUTH_USERNAME")
@@ -38,99 +36,79 @@ app.config["BASIC_AUTH_PASSWORD"] = basic_auth_password
 app.config["BASIC_AUTH_FORCE"] = True
 
 
-def get_and_cache_menu(current_date: str):
+def get_and_cache_menu(current_date: str) -> None:
     """
-    Fetches menu data from an API, filters, and caches it.
+    Fetches and caches breakfast, lunch, and dinner menus based on the current date.
 
     Args:
-        current_date (str): The current date in "YYYY-MM-DD" format.
-
-    Returns:
-        JSON response: A message indicating whether the menu data was updated successfully.
+        current_date (str): The current date in the format "%Y-%m-%d".
     """
-    meal_periods: List[str] = list(MEAL_PERIOD_MAPPING.keys())
+    meal_periods = ["8f7d", "8f73", "8f5b"]  # Breakfast, Lunch, and Dinner
     for period in meal_periods:
-        api_url: str = f"{api_base_url}{period}?platform=0&date={current_date}"
+        api_url = api_base_url + period + "?platform=0&date=" + current_date
         menu = filter_food_items(api_url)
-        meal_name: str = MEAL_PERIOD_MAPPING.get(period, "")
-        cached_data[meal_name] = menu
-
-    return jsonify({"message": "Menu data updated successfully."}), 200
+        cached_menus[period] = menu
 
 
-'''
-@app.route("/update-menus", methods=["POST"])
-def update_buckets() -> None:
+def get_breakfast():
     """
-    Updates the buckets with the cached menu data.
+    Retrieves the cached breakfast menu.
 
     Returns:
-        None
+        cached_menus.get("8f7d", None): The breakfast menu data or None if not cached.
     """
-    get_and_cache_menu(datetime.now(new_york).strftime("%Y-%m-%d"))
-    if cached_data:
-        bucket = storage_client.get_bucket(bucket_name)
-        for meal_period, menu_data in cached_data.items():
-            blob = bucket.blob(f"{meal_period}/menu.json")
-            blob.upload_from_string(json.dumps(menu_data))
+    return cached_menus.get("8f7d", None)
 
 
-def get_bucket_data(meal_period: str):
+def get_lunch():
     """
-    Fetches and returns the menu data from the buckets. If the file is empty or doesn't exist,
-    triggers the update of menus.
+    Retrieves the cached lunch menu.
+
+    Returns:
+        cached_menus.get("8f73", None): The lunch menu data or None if not cached.
+    """
+    return cached_menus.get("8f73", None)
+
+
+def get_dinner():
+    """
+    Retrieves the cached dinner menu.
+
+    Returns:
+        cached_menus.get("8f5b", None): The dinner menu data or None if not cached.
+    """
+    return cached_menus.get("8f5b", None)
+
+
+def filter_food_items(api_url: str):
+    """
+    Fetches and filters food items from the provided API URL.
 
     Args:
-        meal_period (str): The meal period identifier (breakfast, lunch, dinner).
+        api_url (str): The API URL to fetch menu data.
 
     Returns:
-        json: The menu data or an error message.
+        period_name, structured_data: A tuple containing the meal period name and structured menu data.
     """
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(f"{meal_period}/menu.json")
-    menu_data = blob.download_as_string()
+    menu_data = requests.get(api_url).json()
+    structured_data = []
 
-    if not menu_data:
-        update_menus_url = "https://api.njitbytes.co/update-menus"
-        response = requests.post(update_menus_url)
-        if response.status_code == 200:
-            return "Menu data updated successfully. Please retry to fetch the menu."
-        else:
-            return "Failed to update menu data. Please try again later."
+    periods = menu_data.get("menu", {}).get("periods", {})
+    period_name = periods.get("name")
 
-    return json.loads(menu_data)
-'''
-
-
-def filter_food_items(api_url: str) -> Tuple[str, List[Dict[str, Any]]]:
-    """
-    Fetches and filters food items from an API.
-
-    Args:
-        api_url (str): The API URL for menu data.
-
-    Returns:
-        tuple: A tuple containing period name (str) and structured menu data (list of dictionaries).
-    """
-    menu_data: Dict[str, Any] = requests.get(api_url).json()
-    structured_data: List[Dict[str, Any]] = []
-
-    periods: Dict[str, Any] = menu_data.get("menu", {}).get("periods", {})
-    period_name: str = periods.get("name")
-
-    categories: List[Dict[str, Any]] = periods.get("categories", [])
+    categories = periods.get("categories", [])
     for category in categories:
-        name: str = category.get("name")
-        items: List[Dict[str, Any]] = category.get("items", [])
-        category_info: Dict[str, Any] = {"name": name, "items": []}
+        name = category.get("name")
+        items = category.get("items", [])
+        category_info = {"name": name, "items": []}
 
         for item in items:
-            item_name: str = item.get("name")
-            description: str = item.get("desc", "")
-            portion: str = item.get("portion", "")
-            calories: int = item.get("calories", 0)
+            item_name = item.get("name")
+            description = item.get("desc", "")
+            portion = item.get("portion", "")
+            calories = item.get("calories", 0)
 
-            item_info: Dict[str, Any] = {
+            item_info = {
                 "Item Name": item_name,
                 "Description": description,
                 "Portion": portion,
@@ -141,6 +119,92 @@ def filter_food_items(api_url: str) -> Tuple[str, List[Dict[str, Any]]]:
         structured_data.append(category_info)
 
     return period_name, structured_data
+
+
+@app.route("/update-menu", methods=["POST"])
+def update_menu():
+    current_date = datetime.now(new_york).strftime("%Y-%m-%d")
+    api_base_url = "https://api.dineoncampus.com/v1/location/615f4f93a9f13a32678e5feb/periods/64ecff62351d53075fcb"
+    get_and_cache_menu(api_base_url, current_date)
+    breakfast_menu = get_breakfast()
+    lunch_menu = get_lunch()
+    dinner_menu = get_dinner()
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket("gds-menu")
+    breakfast_blob = bucket.blob("breakfast_menu.json")
+    lunch_blob = bucket.blob("lunch_menu.json")
+    dinner_blob = bucket.blob("dinner_menu.json")
+
+    breakfast_blob.upload_from_string(json.dumps(breakfast_menu))
+    lunch_blob.upload_from_string(json.dumps(lunch_menu))
+    dinner_blob.upload_from_string(json.dumps(dinner_menu))
+
+    return "Menu updated", 200
+
+
+def read_from_bucket(blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket("gds-menu")
+    blob = bucket.blob(blob_name)
+    return blob.download_as_string()
+
+
+@app.route("/get-filtered-food-items")
+def get_filtered_food_items():
+    current_time = datetime.now(new_york).time()
+    # current_date = datetime.now(new_york).strftime("%Y-%m-%d")
+    current_time = datetime.now(new_york).time()
+    now = datetime.now(new_york)
+    breakfast_menu = json.loads(read_from_bucket("breakfast_menu.json"))
+    lunch_menu = json.loads(read_from_bucket("lunch_menu.json"))
+    dinner_menu = json.loads(read_from_bucket("dinner_menu.json"))
+
+    if 0 <= now.weekday() <= 4:
+        if (
+            datetime.strptime("07:00:00", "%H:%M:%S").time()
+            <= current_time
+            < datetime.strptime("09:59:59", "%H:%M:%S").time()
+        ):
+            return breakfast_menu
+        elif (
+            datetime.strptime("10:00:00", "%H:%M:%S").time()
+            <= current_time
+            < datetime.strptime("15:59:59", "%H:%M:%S").time()
+        ):
+            return lunch_menu
+        elif (
+            datetime.strptime("16:00:00", "%H:%M:%S").time()
+            <= current_time
+            < datetime.strptime("22:00:00", "%H:%M:%S").time()
+        ):
+            return dinner_menu
+        else:
+            print("GDS is closed :(")
+            return json.dumps({})
+
+    else:
+        if (
+            datetime.strptime("10:00:00", "%H:%M:%S").time()
+            <= current_time
+            < datetime.strptime("11:59:59", "%H:%M:%S").time()
+        ):
+            return breakfast_menu
+        elif (
+            datetime.strptime("12:00:00", "%H:%M:%S").time()
+            <= current_time
+            < datetime.strptime("14:59:59", "%H:%M:%S").time()
+        ):
+            return lunch_menu
+        elif (
+            datetime.strptime("15:00:00", "%H:%M:%S").time()
+            <= current_time
+            < datetime.strptime("20:00:00", "%H:%M:%S").time()
+        ):
+            return dinner_menu
+        else:
+            print("GDS is closed :(")
+            return json.dumps({})
 
 
 @app.route("/<meal_period>", methods=["GET"])
@@ -155,27 +219,12 @@ def get_menu_endpoint(meal_period: str):
     Returns:
         JSON response: The menu data or an error message.
     """
-    menu = jsonify(cached_data.get(meal_period))
+    menu = jsonify(cached_menus.get(meal_period))
     if not menu:
         get_and_cache_menu(datetime.now(new_york).strftime("%Y-%m-%d"))
-        menu = jsonify(cached_data.get(meal_period))
+        menu = jsonify(cached_menus.get(meal_period))
     return menu
 
 
-# Error handler for unauthorized access
-@app.errorhandler(401)
-def unauthorized(e: Exception) -> Tuple[str, int]:
-    """
-    Error handler for unauthorized access.
-
-    Returns:
-        JSON response: An error message for unauthorized access.
-    """
-    return (
-        jsonify({"error": "Unauthorized access. Please provide valid credentials."}),
-        401,
-    )
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
