2adfdf65c8fc9a5e493c1c35ce0fa8f64e99132c
Julian Lechner <jll38@njit.edu>
2023-11-05 23:20:17 -0500

    read json from chatgpt

--- frontend/src/pages/Meals.js
+++ frontend/src/pages/Meals.js
@@ -5,18 +5,36 @@ import { Button } from "@mui/joy";
 import React from "react";
 import axios from "axios";
 import { bruh, bruh_lunch } from "../tmp";
-import { Tabs, TabList, Tab, TabPanel, Select, MenuItem } from "@mui/joy";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  Select,
+  MenuItem,
+  Card,
+  Skeleton,
+  AspectRatio,
+  Typography,
+} from "@mui/joy";
 import { useState, useEffect } from "react";
 import { getDailyCals } from "../utils/macros";
 import OpenAI from "openai";
 import { json } from "react-router";
+import { isJson } from "../utils/parser";
 
 export function Meals({}) {
   const [breakfast, setBreakfast] = useState(null);
   const [lunch, setLunch] = useState(null);
   const [dinner, setDinner] = useState(null);
-  const [selection, setSelection] = useState("breakfast");
   const [responseData, setResponseData] = useState(null);
+  const [errorState, setErrorState] = useState(null);
+
+  useEffect(() => {
+    console.error(errorState);
+  }, [errorState]);
+
+  /*TODO: Add logic to limit daily meal requests*/
 
   useEffect(() => {
     let url;
@@ -55,7 +73,7 @@ export function Meals({}) {
       })
       .then((response) => {
         const res = response.data;
-        setLunch(res);
+        setLunch(res.slice(0, 16388));
       })
       .catch((error) => {});
   }, []);
@@ -97,106 +115,44 @@ export function Meals({}) {
       menu = dinner;
     }
     console.log("contacting openai...");
-    const response = await openai.completions.create({
-      model: "gpt-3.5-turbo-instruct",
-      prompt: `For your response, do not elaborate & keep it brief. I am a ${
-        user.age
-      } year old ${user.sex}, standing at ${user.heightFeet}ft ${
-        user.heightInches
-      }in tall, weighing in at ${
-        user.weight
-      } with a recommended caloric intake of ${getDailyCals()}.  My dietary restriction is the following: ${
-        user.restrictions
-      }. My dietary preference is the following: ${
-        user.preferences
-      }. Give me 2 meal combinations from my dining hall menu, focus on high protein and give measurements for protein, carbs, fat, and calories (preferably at the start) for the total meal, specify the serving per ingredient, and am looking to ${
-        user.goal
-      }. Here is the dining hall menu for ${selection}. ${JSON.stringify(
-        bruh[1]
-      )}`,
-      max_tokens: 200,
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an expert dietician that follows food preferences and dietary restrictions. You also only give information in a JSON object. Follow this format without deviation {'meals': [{'meal1': [{ingredient: {'name': '(fill)', 'calories': '(fill)','carbs': '(fill)', 'protein': '(fill)', 'fat': '(fill)', 'serving size': '(fill)'}}]}]}",
+        },
+        {
+          role: "user",
+          content: `I am a ${user.age} year old ${user.sex}, standing at ${
+            user.heightFeet
+          }ft ${user.heightInches}in tall, weighing in at ${
+            user.weight
+          } pounds with a recommended caloric intake of ${getDailyCals()}. My dietary restriction is the following: ${
+            user.restrictions
+          }. My dietary preference is the following: ${
+            user.preferences
+          }. Give me 2 meal combinations from my dining hall menu, focus on high protein and give measurements for protein, carbs, fat, and calories (preferably at the start) for the total meal, specify the serving per ingredient, and am looking to ${
+            user.goal
+          }. Here is the dining hall menu for ${selection}. ${JSON.stringify(
+            menu
+          ).slice(0, 11288)}`,
+        },
+      ],
+      max_tokens: 400,
     });
-    console.log(response.choices[0].text);
-    setResponseData(response.choices[0].text);
+    const mealStr = response.choices[0].message.content;
+    try {
+      setResponseData(JSON.parse(mealStr));
+    } catch (e) {
+      console.error("Something went wrong! Please try again later");
+    }
   };
   useEffect(() => {
     if (localStorage.getItem("byte_quizStatus") === null)
       window.location.assign("/");
   });
-
-  /*useEffect(() => {
-    let url;
-    if (process.env.REACT_APP_ENV === "development") {
-      url = "https://corsproxy.io/?https://api.njitbytes.co/breakfast";
-    } else {
-      url = "https://api.njitbytes.co/breakfast";
-    }
-    axios
-      .get(url, {
-        auth: {
-          username: process.env.REACT_APP_BASIC_AUTH_USERNAME, // This should be your basic auth username
-          password: process.env.REACT_APP_BASIC_AUTH_PASSWORD, // This should be your basic auth password
-        },
-      })
-      .then((response) => {
-        const res = response.data;
-        setBreakfast(res)
-      })
-      .catch((error) => {
-        
-        console.log(error);
-
-      });
-  }, []);
-
-  useEffect(() => {
-    let url;
-    if (process.env.REACT_APP_ENV === "development") {
-      url = "https://corsproxy.io/?https://api.njitbytes.co/lunch";
-    } else {
-      url = "https://api.njitbytes.co/lunch";
-    }
-    axios
-      .get(url, {
-        auth: {
-          username: process.env.REACT_APP_BASIC_AUTH_USERNAME, // This should be your basic auth username
-          password: process.env.REACT_APP_BASIC_AUTH_PASSWORD, // This should be your basic auth password
-        },
-      })
-      .then((response) => {
-        const res = response.data;
-        setLunch(res)
-      })
-      .catch((error) => {
-        console.log(error);
-
-      });
-  }, []);
-
-  useEffect(() => {
-    let url;
-    if (process.env.REACT_APP_ENV === "development") {
-      url = "https://corsproxy.io/?https://api.njitbytes.co/dinner";
-    } else {
-      url = "https://api.njitbytes.co/dinner";
-    }
-    axios
-      .get(url, {
-        auth: {
-          username: process.env.REACT_APP_BASIC_AUTH_USERNAME, // This should be your basic auth username
-          password: process.env.REACT_APP_BASIC_AUTH_PASSWORD, // This should be your basic auth password
-        },
-      })
-      .then((response) => {
-        const res = response.data;
-        setDinner(res)
-      })
-      .catch((error) => {
-        console.log(error);
-
-      });
-  }, []);
-*/
 
   return localStorage.getItem("byte_quizStatus") !== null ? (
     <div>
@@ -206,6 +162,7 @@ export function Meals({}) {
         <div className="sm:mt-[5rem] w-full flex items-center gap-4 justify-center">
           <Button
             onClick={() => {
+              setResponseData(null);
               getResponse("breakfast");
             }}
           >
@@ -213,6 +170,7 @@ export function Meals({}) {
           </Button>
           <Button
             onClick={() => {
+              setResponseData(null);
               getResponse("lunch");
             }}
           >
@@ -220,15 +178,63 @@ export function Meals({}) {
           </Button>
           <Button
             onClick={() => {
+              setResponseData(null);
               getResponse("dinner");
             }}
           >
             Get Dinner Meal
           </Button>
         </div>
-        <div className="p-0 max-w-[80rem]">
-          <pre>{responseData}</pre>
-        </div>
+        {responseData &&
+          responseData.meals.map((mealObject, index) => {
+            const mealKey = `meal${index + 1}`; // Constructing the meal key based on the index
+            const meal = mealObject[mealKey]; // Accessing the correct meal array
+            return (
+              <Card
+                variant="outlined"
+                sx={{ width: 343, display: "flex", gap: 2 }}
+              >
+                <AspectRatio ratio="21/9">
+                  {/* ...assuming you want to put something here... */}
+                </AspectRatio>
+                <Typography>
+                  {meal &&
+                    meal.map((ingredientObject) => {
+                      const ingredient = ingredientObject.ingredient;
+                      return (
+                        <div key={ingredient.name}>
+                          <h3>{ingredient.name}</h3>
+                          <div className="text-primary-accent">Serving size: {ingredient["serving size"]}</div>
+                          {/* Added a key for React list rendering */}
+                          {`${ingredient.calories}cals ${ingredient.carbs} carbs ${ingredient.protein} protein ${ingredient.fat} fat`}
+                          <br /> {/* Added line break for readability */}
+                        </div>
+                      );
+                    })}
+                </Typography>
+              </Card>
+            );
+          })}
+
+        {!responseData && (
+          <Card variant="outlined" sx={{ width: 343, display: "flex", gap: 2 }}>
+            <AspectRatio ratio="21/9">
+              <Skeleton variant="overlay">
+                <img
+                  alt=""
+                  src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+                />
+              </Skeleton>
+            </AspectRatio>
+            <Typography>
+              <Skeleton>
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries.
+              </Skeleton>
+            </Typography>
+          </Card>
+        )}
+        <div className="p-0 max-w-[80rem]"></div>
       </div>
 
       <Footer />
