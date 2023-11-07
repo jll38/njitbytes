fd90afe82b3e957423ac3a1a424ef0255f1de7b2
Julian Lechner <jll38@njit.edu>
2023-11-07 12:10:32 -0500

    consistent AI output as JSON

--- frontend/src/pages/Meals.js
+++ frontend/src/pages/Meals.js
@@ -6,7 +6,8 @@ import { Button } from "@mui/joy";
 import React from "react";
 import axios from "axios";
 import { Chart } from "react-google-charts";
-import CloseIcon from '@mui/icons-material/Close';
+import Snackbar from "@mui/joy/Snackbar";
+
 import {
   Tabs,
   TabList,
@@ -30,7 +31,6 @@ export function Meals({}) {
   const [responseData, setResponseData] = useState(null);
   const [errorState, setErrorState] = useState(null);
   const [loading, setLoading] = useState(false);
-
   const pieChartOptions = {
     legend: "none",
     pieSliceText: "none",
@@ -38,7 +38,6 @@ export function Meals({}) {
     tooltip: { trigger: "hover" },
     width: 100,
     height: 150,
-
 
     chartArea: {
       backgroundColor: {
@@ -56,6 +55,14 @@ export function Meals({}) {
       tableCell: "pie-chart-box",
     },
   };
+
+  const macros = {
+    calories: 0,
+    protein: 0,
+    fat: 0,
+    carbs: 0,
+  };
+
   const loadingMessages = [
     "Thinking...",
     "Looking at the GDS menu...",
@@ -166,33 +173,34 @@ export function Meals({}) {
           {
             role: "system",
             content:
-              "You are an expert dietician that follows food preferences and dietary restrictions. You also only give information in a JSON object. Follow this format without deviation {'meals': [{'meal1': [{ingredient: {'name': '(fill)', 'calories': '(fill)','carbs': '(fill)', 'protein': '(fill)', 'fat': '(fill)', 'serving size': '(fill)'}}]}]}",
+              'Generate a JSON object for a single meal plan. The JSON object should contain an array of no more than 4 ingredients for the meal, each with its nutritional information and number of servings you suggest in relation to other ingredients. If protein, carb, fat informaiton isn\'t provided, approximate. The JSON structure must follow this specific format without deviation: {"meal": [{"ingredient": {"name": "(fill)", "calories": "(fill)", "carbs": "(fill)", "protein": "(fill)", "fat": "(fill)", "serving size", "num_servings": "(fill)"}}]}',
           },
           {
             role: "user",
-            content: `I am a ${user.age} year old ${user.sex}, standing at ${
-              user.heightFeet
-            }ft ${user.heightInches}in tall, weighing in at ${
+            content: `Generate a meal, focus on high protein and values for each macro asked for, specify the serving per ingredient. User information:
+            ${user.age} years old, ${user.sex}, ${user.heightFeet}ft ${
+              user.heightInches
+            }in, ${
               user.weight
-            } pounds with a recommended caloric intake of ${getDailyCals()}. My dietary restriction is the following: ${
+            } pounds, recommended caloric intake of ${getDailyCals()}, ${
               user.restrictions
-            }. My dietary preference is the following: ${
-              user.preferences
-            }. Give me 2 meal combinations from my dining hall menu, focus on high protein and give measurements for protein, carbs, fat, and calories (preferably at the start) for the total meal, specify the serving per ingredient, and am looking to ${
+            }, dietary preference is ${user.preferences}, wants to ${
               user.goal
-            }. Here is the dining hall menu for ${selection}. ${JSON.stringify(
-              menu
-            ).slice(0, 11288)}`,
+            }. Here is the menu ${selection}. ${JSON.stringify(menu).slice(
+              0,
+              11288
+            )}`,
           },
         ],
         max_tokens: 400,
       });
     } catch (err) {
       console.error("ChatGPT failed to respond");
-      setLoading(false)
+      setLoading(false);
       setErrorState("ChatGPT failed to respond");
     }
     const mealStr = response.choices[0].message.content;
+    console.log(mealStr);
     setLoading(false);
     try {
       setResponseData(JSON.parse(mealStr));
@@ -238,82 +246,107 @@ export function Meals({}) {
             Get Dinner Meal
           </Button>
         </div>
+        <div className="opacity-[50%]">
+          * Calories and macros are adjusted by number of servings
+        </div>
         <div className="w-full flex justify-center p-[2em]">
           <div className="flex w-[75%] justify-around">
-            {responseData &&
-              responseData.meals.map((mealObject, index) => {
-                const mealKey = `meal${index + 1}`; // Constructing the meal key based on the index
-                const meal = mealObject[mealKey]; // Accessing the correct meal array
-                return (
-                  <Card
-                    variant="outlined"
-                    sx={{ width: 343, display: "flex", gap: 2 }}
-                  >
-                    <Typography>
-                      {meal &&
-                        meal.map((ingredientObject) => {
-                          const ingredient = ingredientObject.ingredient;
-                          const data = [
-                            ["Macros", "Grams"],
-                            [
-                              "Protein",
-                              Number.parseInt(ingredient.protein.slice(0, -1)),
-                            ],
-                            [
-                              "Carbs",
-                              Number.parseInt(ingredient.carbs.slice(0, -1)),
-                            ],
-                            [
-                              "Fat",
-                              Number.parseInt(ingredient.fat.slice(0, -1)),
-                            ],
-                          ];
-                          return (
-                            <div className="flex" key={ingredient.name}>
-                              <div className="">
-                                <h3 className="w-[14rem]">{ingredient.name}</h3>
-                                <div className="text-primary-accent">
-                                  Serving size: {ingredient["serving size"]}
-                                </div>
-                                <div className="flex items-center gap-2">
-                                  <div className="w-[4px] h-[4px] bg-[#3366cc]"></div>
-                                  {ingredient.protein} Protein
-                                </div>
-                                <div className="flex items-center gap-2">
-                                  <div className="w-[4px] h-[4px] bg-[#dc3911]"></div>
-                                  {ingredient.carbs} Carbs
-                                </div>
-                                <div className="flex items-center gap-2">
-                                  <div className="w-[4px] h-[4px] bg-[#ff9900]"></div>
-                                  {ingredient.fat} Fat
-                                </div>
-                              </div>
-                              <div className="-mt-10 -ml-[0rem]  ">
-                                <div className="z-20">
-                                  <Chart
-                                    chartType="PieChart"
-                                    data={data}
-                                    options={pieChartOptions}
-                                    width={"100%"}
-                                    height={"125px"}
-                                  ></Chart>
-                                </div>
-                                <div className="relative z-0">
-                                  <div className="absolute  w-[75px] h-[75px] rounded-[75px] -top-[5.4rem] left-[0.75rem] flex justify-center items-center font-semibold text-[1.25em]">
-                                    {ingredient.calories}
-                                  </div>
-                                </div>
+            <>
+              <Card
+                variant="outlined"
+                sx={{ width: 343, display: "flex", gap: 2 }}
+              >
+                <Typography>
+                  {responseData &&
+                    responseData.meal.map((ingredientObject) => {
+                      const ingredient = ingredientObject.ingredient;
+                      const data = [
+                        ["Macros", "Grams"],
+                        [
+                          "Protein",
+                          Number.parseInt(ingredient.protein.slice(0, -1)),
+                        ],
+                        [
+                          "Carbs",
+                          Number.parseInt(ingredient.carbs.slice(0, -1)),
+                        ],
+                        ["Fat", Number.parseInt(ingredient.fat.slice(0, -1))],
+                      ];
+                      macros.calories +=
+                        Number.parseInt(ingredient.calories) *
+                        Number.parseInt(ingredient["num_servings"]);
+                      macros.protein +=
+                        Number.parseInt(ingredient.protein.slice(0, -1)) *
+                        Number.parseInt(ingredient["num_servings"]);
+                      macros.fat +=
+                        Number.parseInt(ingredient.fat.slice(0, -1)) *
+                        Number.parseInt(ingredient["num_servings"]);
+                      macros.carbs +=
+                        Number.parseInt(ingredient.carbs.slice(0, -1)) *
+                        Number.parseInt(ingredient["num_servings"]);
+
+                      return (
+                        <div className="flex" key={ingredient.name}>
+                          <div className="">
+                            <h3 className="w-[14rem]">{ingredient.name}</h3>
+                            <div className="text-primary-accent">
+                              Serving size: {ingredient["serving size"]} <br />
+                              Servings: {ingredient["num_servings"]}
+                            </div>
+                            <div className="flex items-center gap-2">
+                              <div className="w-[4px] h-[4px] bg-[#3366cc]"></div>
+                              {Number.parseInt(ingredient.protein) *
+                                Number.parseInt(
+                                  ingredient["num_servings"]
+                                )}g
+                              Protein
+                            </div>
+                            <div className="flex items-center gap-2">
+                              <div className="w-[4px] h-[4px] bg-[#dc3911]"></div>
+                              {Number.parseInt(ingredient.carbs) *
+                                Number.parseInt(
+                                  ingredient["num_servings"]
+                                )}g Carbs
+                            </div>
+                            <div className="flex items-center gap-2">
+                              <div className="w-[4px] h-[4px] bg-[#ff9900]"></div>
+                              {Number.parseInt(ingredient.fat) *
+                                Number.parseInt(
+                                  ingredient["num_servings"]
+                                )}g Fat 
+                            </div>
+                          </div>
+                          <div className="-mt-10 -ml-[0rem]  ">
+                            <div className="z-20">
+                              <Chart
+                                chartType="PieChart"
+                                data={data}
+                                options={pieChartOptions}
+                                width={"100%"}
+                                height={"125px"}
+                              ></Chart>
+                            </div>
+                            <div className="relative z-0">
+                              <div className="absolute  w-[75px] h-[75px] rounded-[75px] -top-[5.4rem] left-[0.75rem] flex justify-center items-center font-semibold text-[1.25em]">
+                                {ingredient.calories *
+                                  Number.parseInt(ingredient["num_servings"])}
                               </div>
                             </div>
-                          );
-                        })}
-                    </Typography>
-                  </Card>
-                );
-              })}
-
+                          </div>
+                        </div>
+                      );
+                    })}
+                  <div>{macros.calories} Calories</div>
+                  <div>{macros.protein}g Protein</div>
+                  <div>{macros.carbs}g Carbs</div>
+                  <div>{macros.fat}g Fat</div>
+                </Typography>
+              </Card>
+            </>
             {loading && <div>Loading...</div>}
-            {errorState && <div className="text-destructive">X {errorState}</div>}
+            {errorState && (
+              <div className="text-destructive">X {errorState}</div>
+            )}
           </div>
         </div>
       </div>
