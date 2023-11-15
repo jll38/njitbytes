a91abaed0fb09c47beb5f0b1831977b6c7efad7e
Vladimir-G4 <vgutierrez.four@gmail.com>
2023-11-15 02:48:07 -0500

    Updated Home UI

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,34 +1,46 @@
-import React from "react";
-import Chip from "@mui/joy/Chip";
-import { isOpenNow } from "../utils/timehandler";
+import React from 'react';
+import { Chip } from '@mui/joy';
+import { isOpenNow } from '../utils/timehandler';
 
-export function Logo({ includeChip = true }) {
+function LogoImage() {
+  return (
+    <img
+      className="mb-2 w-full max-w-[150px] h-auto"
+      src="/images/njitbytes-transparent.png"
+      alt="njitbytes logo"
+    />
+  );
+}
+
+function LogoContent({ includeChip }) {
   const isOpen = isOpenNow();
 
   return (
-    <a  href="/">
-      <div id="logo" className="flex flex-col items-center leading-10 ">
-        <img
-          className="mb-2 w-full max-w-[150px] h-auto"
-          src="/images/njitbytes-transparent.png"
-          alt="njitbytes logo alt"
-        />
-        <h1 className="text-[2em] sm:text-[4em]">NJITBytes</h1>
-        <div className="mt-4">Your NJIT Dining Companion</div>
-        {includeChip && (
-          <Chip
-            className="mt-4"
-            startDecorator={
-              <div
-                className="h-[10px] w-[10px] rounded-[10px]"
-                style={{ backgroundColor: isOpen.color }}
-              ></div>
-            }
-          >
-            <p className="text-[.75em]">{isOpen.message}</p>
-          </Chip>
-        )}
-      </div>
+    <div id="logo" className="flex flex-col items-center leading-10">
+      <LogoImage />
+      <h1 className="text-[2em] sm:text-[4em]">NJITBytes</h1>
+      <div className="mt-4">Your NJIT Dining Companion</div>
+      {includeChip && (
+        <Chip
+          className="mt-4"
+          startDecorator={
+            <div
+              className="h-[10px] w-[10px] rounded-[10px]"
+              style={{ backgroundColor: isOpen.color }}
+            ></div>
+          }
+        >
+          <p className="text-[.75em]">{isOpen.message}</p>
+        </Chip>
+      )}
+    </div>
+  );
+}
+
+export function Logo({ includeChip = true }) {
+  return (
+    <a href="/">
+      <LogoContent includeChip={includeChip} />
     </a>
   );
 }
