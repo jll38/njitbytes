f595a6bf6c8cd7c49f72d531a5da4ba2dbe2a475
Vladimir-G4 <vgutierrez.four@gmail.com>
2023-11-15 03:21:50 -0500

    Updated Settings UI

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import Form from "../components/questions/Form";
-import { Button, Box } from "@mui/joy";
+import { Button } from "@mui/joy";
 import { Logo } from "../components/Logo";
 import { Footer } from "../components/Footer";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
@@ -10,9 +10,9 @@ import SettingsIcon from "@mui/icons-material/Settings";
 export function Home({ quizStatus, setQuizStatus }) {
   const isMobile = window.innerWidth <= 600;
   const handStyle = {
-    display: 'inline-block',
-    animation: 'wave 2s infinite',
-    transformOrigin: '70% 70%',
+    display: "inline-block",
+    animation: "wave 2s infinite",
+    transformOrigin: "70% 70%",
   };
 
   const keyframes = `
@@ -35,99 +35,142 @@ export function Home({ quizStatus, setQuizStatus }) {
     }
   `;
 
+  useEffect(() => {
+    if (isMobile) {
+      document.documentElement.style.overflowY = "hidden";
+      document.body.style.overflowY = "hidden";
+
+      // Remove styles on component unmount
+      return () => {
+        document.documentElement.style.overflowY = "auto";
+        document.body.style.overflowY = "auto";
+      };
+    }
+  }, [isMobile]);
+
   return (
-    <div style={{
-      padding: '1%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      height: '100vh',
-      justifyContent: 'center',
-      paddingBottom: '10%',
-    }}>
+    <div
+      style={{
+        padding: "1%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100vh",
+        justifyContent: "center",
+        paddingBottom: "10%",
+      }}
+    >
       <Logo />
 
       {quizStatus ? (
-        <div style={{ textAlign: 'center', marginTop: '2%', width: '80%' }}>
-          <div style={{ fontSize: '1.8rem', marginTop: '2%', textAlign: 'center', margin: 'auto' }}>
+        <div style={{ textAlign: "center", marginTop: "2%", width: "80%" }}>
+          <div
+            style={{
+              fontSize: "1.8rem",
+              marginTop: "2%",
+              textAlign: "center",
+              margin: "auto",
+            }}
+          >
             Welcome, {JSON.parse(localStorage.getItem("byte_name"))}
-            <span style={{ marginLeft: '8px' }}>
+            <span style={{ marginLeft: "8px" }}>
               <style>{keyframes}</style>
               <span role="img" aria-label="Waving Hand" style={handStyle}>
                 👋
               </span>
             </span>
           </div>
-          <div style={{ fontSize: '1.2rem', marginTop: '1%' }}>What would you like to do?</div>
-          <div style={{
-  display: 'flex',
-  flexDirection: isMobile ? 'column' : 'row',
-  alignItems: 'center',
-  justifyContent: 'center',
-  marginTop: '5%',
-  gap: '1rem',
-}}>
-  <Button
-    variant="contained"
-    style={{
-      height: '10vh',
-      width: isMobile ? '70vw' : '15vw', // Adjusted width for mobile view
-      maxWidth: '225px', // Adjusted max width for desktop view
-      display: 'flex',
-      flexDirection: 'column',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      transition: 'transform 0.3s',
-      marginBottom: '10px',
-    }}
-    onClick={() => window.location.assign("/menu")}
-    onMouseOver={(e) => (e.currentTarget.style.transform = 'translateY(-8px)')}
-    onMouseOut={(e) => (e.currentTarget.style.transform = 'translateY(0)')}
-  >
-    <MenuBookIcon style={{ fontSize: '2rem' }} />
-    <div style={{ fontSize: '0.8rem', marginTop: '0.5rem' }}>View Menu</div>
-  </Button>
+          <div style={{ fontSize: "1.2rem", marginTop: "1%" }}>
+            What would you like to do?
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: isMobile ? "column" : "row",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "5%",
+              gap: "1rem",
+            }}
+          >
+            <Button
+              variant="contained"
+              style={{
+                height: "10vh",
+                width: isMobile ? "70vw" : "15vw", // Adjusted width for mobile view
+                maxWidth: "225px", // Adjusted max width for desktop view
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                transition: "transform 0.3s",
+                marginBottom: "10px",
+              }}
+              onClick={() => window.location.assign("/menu")}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "translateY(-8px)")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.transform = "translateY(0)")
+              }
+            >
+              <MenuBookIcon style={{ fontSize: "2rem" }} />
+              <div style={{ fontSize: "0.8rem", marginTop: "0.5rem" }}>
+                View Menu
+              </div>
+            </Button>
 
-  <Button
-    variant="contained"
-    style={{
-      height: '10vh',
-      width: isMobile ? '70vw' : '15vw',
-      maxWidth: '225px',
-      display: 'flex',
-      flexDirection: 'column',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      transition: 'transform 0.3s',
-      marginBottom: '10px',
-    }}
-    onClick={() => window.location.assign("/meals")}
-    onMouseOver={(e) => (e.currentTarget.style.transform = 'translateY(-8px)')}
-    onMouseOut={(e) => (e.currentTarget.style.transform = 'translateY(0)')}
-  >
-    <SmartToyIcon style={{ fontSize: '2rem' }} />
-    <div style={{ fontSize: '0.8rem', marginTop: '0.5rem' }}>AI Meal Prep</div>
-  </Button>
+            <Button
+              variant="contained"
+              style={{
+                height: "10vh",
+                width: isMobile ? "70vw" : "15vw",
+                maxWidth: "225px",
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                transition: "transform 0.3s",
+                marginBottom: "10px",
+              }}
+              onClick={() => window.location.assign("/meals")}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "translateY(-8px)")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.transform = "translateY(0)")
+              }
+            >
+              <SmartToyIcon style={{ fontSize: "2rem" }} />
+              <div style={{ fontSize: "0.8rem", marginTop: "0.5rem" }}>
+                AI Meal Prep
+              </div>
+            </Button>
 
-  <Button
-    variant="contained"
-    style={{
-      height: '10vh',
-      width: isMobile ? '70vw' : '15vw',
-      maxWidth: '225px',
-      display: 'flex',
-      flexDirection: 'column',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      transition: 'transform 0.3s',
-      marginBottom: '10px',
-    }}
-    onClick={() => window.location.assign("/settings")}
-    onMouseOver={(e) => (e.currentTarget.style.transform = 'translateY(-8px)')}
-    onMouseOut={(e) => (e.currentTarget.style.transform = 'translateY(0)')}
-  >
-    <SettingsIcon style={{ fontSize: '2rem' }} />
-    <div style={{ fontSize: '0.8rem', marginTop: '0.5rem' }}>Settings</div>
-  </Button>
-</div>
-
+            <Button
+              variant="contained"
+              style={{
+                height: "10vh",
+                width: isMobile ? "70vw" : "15vw",
+                maxWidth: "225px",
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                transition: "transform 0.3s",
+                marginBottom: "10px",
+              }}
+              onClick={() => window.location.assign("/settings")}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "translateY(-8px)")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.transform = "translateY(0)")
+              }
+            >
+              <SettingsIcon style={{ fontSize: "2rem" }} />
+              <div style={{ fontSize: "0.8rem", marginTop: "0.5rem" }}>
+                Settings
+              </div>
+            </Button>
+          </div>
         </div>
       ) : (
         <Form setQuizStatus={setQuizStatus} />
--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -9,73 +9,139 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import { Menu } from "./Menu";
 
-export function Settings() {
-    if(localStorage.getItem("byte_quizStatus") === null) window.location.assign('/');
+export function Settings({}) {
+  const isMobile = window.innerWidth <= 600;
+  if(localStorage.getItem("byte_quizStatus") === null) window.location.assign('/');
+
   return (
     localStorage.getItem("byte_quizStatus") !== null ?
-    <div>
-      <div
-        className="p-[4rem 
-        h-screen w-full flex flex-col justify-center items-center gap-6"
-      >
-        <Logo includeChip={false} />
+    <div
+      style={{
+        padding: "1%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100vh",
+        justifyContent: "center",
+        paddingBottom: "10%",
+      }}
+    >
+      <Logo />
 
-        <div className="flex justify-center gap-4">
-        <Button
-            variant="soft"
-            className="h-[8rem] w-[8rem] flex flex-col"
-            onClick={() => {
+      {(
+        <div style={{ textAlign: "center", marginTop: "2%", width: "80%" }}>
+          <div
+            style={{
+              fontSize: "1.8rem",
+              marginTop: "2%",
+              textAlign: "center",
+              margin: "auto",
+            }}
+          >
+            📃Settings
+          </div>
+          <div style={{ fontSize: "1.2rem", marginTop: "1%" }}>
+            What would you like to do?
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: isMobile ? "column" : "row",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "5%",
+              gap: "1rem",
+            }}
+          >
+            <Button
+              variant="contained"
+              style={{
+                height: "10vh",
+                width: isMobile ? "70vw" : "15vw", // Adjusted width for mobile view
+                maxWidth: "225px", // Adjusted max width for desktop view
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                transition: "transform 0.3s",
+                marginBottom: "10px",
+              }}
+              onClick={() => {
                 window.location.assign("/settings/edit-profile");
             }}
-            sx={{
-              color: "text",
-            }}
-          >
-            <PersonIcon style={{ fontSize: "48px" }} />
-            <div
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "translateY(-8px)")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.transform = "translateY(0)")
+              }
             >
-              Edit Profile
-            </div>
-          </Button>
-          <Button
-            variant="soft"
-            className="h-[8rem] w-[8rem] flex flex-col"
-            onClick={() => {
-              window.location.assign("/menu/favorites");
-            }}
-            sx={{
-              color: "text",
-            }}
-          >
-            <FavoriteIcon style={{ fontSize: "48px" }} />
-            <div
+              <PersonIcon style={{ fontSize: "2rem" }} />
+              <div style={{ fontSize: "0.8rem", marginTop: "0.5rem" }}>
+                Edit Profile
+              </div>
+            </Button>
+
+            <Button
+              variant="contained"
+              style={{
+                height: "10vh",
+                width: isMobile ? "70vw" : "15vw",
+                maxWidth: "225px",
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                transition: "transform 0.3s",
+                marginBottom: "10px",
+              }}
+              onClick={() => {
+                window.location.assign("/menu/favorites");
+              }}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "translateY(-8px)")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.transform = "translateY(0)")
+              }
             >
-              Favorite <br/>Menu Items
-            </div>
-          </Button>
-          <Button
-            variant="soft"
-            className="h-[8rem] w-[8rem] flex flex-col"
-            onClick={() => {
-              window.location.assign("/");
-              localStorage.clear();
-            }}
-            sx={{
-              color: "text",
-            }}
-          >
-            <SettingsIcon style={{ fontSize: "48px" }} />
-            <div
+              <FavoriteIcon style={{ fontSize: "2rem" }} />
+              <div style={{ fontSize: "0.8rem", marginTop: "0.5rem" }}>
+                Favorite Menu Items
+              </div>
+            </Button>
+
+            <Button
+              variant="contained"
+              style={{
+                height: "10vh",
+                width: isMobile ? "70vw" : "15vw",
+                maxWidth: "225px",
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                transition: "transform 0.3s",
+                marginBottom: "10px",
+              }}
+              onClick={() => {
+                window.location.assign("/");
+                localStorage.clear();
+              }}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "translateY(-8px)")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.transform = "translateY(0)")
+              }
             >
-              Erase User Information
-            </div>
-          </Button>
+              <SettingsIcon style={{ fontSize: "2rem" }} />
+              <div style={{ fontSize: "0.8rem", marginTop: "0.5rem" }}>
+                Erase User Information
+              </div>
+            </Button>
+          </div>
         </div>
-      </div>
-      
+      )}
+
       <Footer />
-
-
     </div> : <></>
   );
 }
